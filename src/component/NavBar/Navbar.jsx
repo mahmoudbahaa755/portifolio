e18ae2f5bfe9dc9987129bb3bffa51343a0efc82
mahmoudbahaa755 <mahmoudbaha755@gmail.com>
2023-10-22 21:32:52 +0300

    main

--- src/component/NavBar/Navbar.jsx
+++ src/component/NavBar/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                   width={66}
                   height={66}
                 />
-
+          {/* <div className={`${style.name} `}>{item.name}</div> */}
                   </div>
               </Link>
             ))}
